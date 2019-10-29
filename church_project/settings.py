b5c3754419921404e66e86029b6e330264c4f815
nijap <nijap@techversantinfotech.com>
2019-10-29 11:46:53 +0530

    postgres app

--- church_project/settings.py
+++ church_project/settings.py
@@ -91,13 +91,24 @@ WSGI_APPLICATION = 'church_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'jobportal_db',
+    'USER': 'jobportal',
+    'PASSWORD': 'jobportal!@#',
+    'HOST': 'localhost',
+    'PORT': '', # Set to empty string for default.
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
