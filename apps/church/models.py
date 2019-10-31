78f8728d5bea1995db0575827d00b4455118aa8f
nijap <nijap@techversantinfotech.com>
2019-10-31 12:16:10 +0530

    admin profile

--- apps/church/models.py
+++ apps/church/models.py
@@ -112,6 +112,8 @@ class Notification(models.Model):
 
 
 
+
+
 class AdminProfile(models.Model):
 	user=models.OneToOneField(User,on_delete=models.CASCADE)
 	mobile_number=models.CharField(null=True,blank=True,max_length=100)
