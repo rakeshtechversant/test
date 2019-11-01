c9a05644396516f8b4b5ddae12603225167ff14b
nijap <nijap@techversantinfotech.com>
2019-11-01 14:48:42 +0530

    images

--- church_project/settings.py
+++ church_project/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -208,3 +209,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT = os.path.join (BASE_DIR, 'cards')
+MEDIA_URL = os.path.join(BASE_DIR, 'cards/')
