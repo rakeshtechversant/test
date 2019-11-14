3e4e8ac106ab54b5957e7f85b0d703b1b43a9a1a
Midhun PC <midhun.techversant@gmail.com>
2019-11-14 10:46:47 +0530

    OTP verify response change

--- apps/api/views.py
+++ apps/api/views.py
@@ -265,7 +265,8 @@ class OtpVerifyViewSet(CreateAPIView):
                             'mobile': member.phone_no_secondary_user,
                             'user_type': 'SECONDARY',
                             'name': member.member_name,
-                            'primary_name':member.primary_user_id.name
+                            'primary_name':member.primary_user_id.name,
+                            'primary_mobile_number':member.primary_user_id.phone_no_primary
                         }
                 except Members.DoesNotExist:
                     return Response({'success': False, 'message': 'Secondary account does not exist'}, status=HTTP_404_NOT_FOUND)
