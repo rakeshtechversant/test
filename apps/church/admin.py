c749bbbdcc54a0703f79c20c41e0c18591e1c55f
Midhun PC <midhun.techversant@gmail.com>
2019-11-20 16:42:29 +0530

    Admin view change

--- apps/church/admin.py
+++ apps/church/admin.py
@@ -11,7 +11,8 @@ from import_export import resources
 from django.http import HttpResponse
 import csv
 class FileAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    list_display = ('name', 'phone_no_primary')
+    search_fields = ['name','phone_no_primary']
     actions = ['download_csv']
 
     def download_csv(self, request, queryset):
@@ -32,7 +33,16 @@ class FileAdmin(admin.ModelAdmin):
 
 
 class MemeberAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    list_display = ('member_name','phone_no_secondary_user')
+    search_fields = ['member_name','phone_no_secondary_user']
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image','category')
+    search_fields = ['image','category']
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('created_by_admin','created_by_primary','created_by_secondary','message')
+    search_fields = ['created_by_admin','created_by_primary','created_by_secondary','message']
 
 admin.site.register(UserProfile)
 admin.site.register(PrayerGroup)
@@ -40,8 +50,8 @@ admin.site.register(Family)
 admin.site.register(Notice)
 admin.site.register(ChurchDetails)
 admin.site.register(OtpModels)
-admin.site.register(Notification)
-admin.site.register(Images)
+admin.site.register(Notification,NotificationAdmin)
+admin.site.register(Images,ImageAdmin)
 admin.site.register(FileUpload,FileAdmin)
 admin.site.register(Members,MemeberAdmin)
 admin.site.register(NoticeBereavement)
