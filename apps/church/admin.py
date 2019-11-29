0d397adbc0b2f35956946381c1f97654e31e0bf3
nijap <nijap@techversantinfotech.com>
2019-11-29 10:04:29 +0530

    admin

--- apps/church/admin.py
+++ apps/church/admin.py
@@ -4,13 +4,12 @@ from django.contrib import admin
 # Register your models here.
 from apps.church.models import NoticeBereavement, Members, UserProfile, PrayerGroup, Notice, Family, ChurchDetails, \
     OtpModels, FileUpload, Notification, Images, Occupation, MemberType, NoticeReadSecondary, NoticeReadPrimary, \
-    NoticeReadAdmin, ViewRequestNumber,PrivacyPolicy,PhoneVersion
+    NoticeReadAdmin, ViewRequestNumber, PrivacyPolicy, PhoneVersion
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin, ImportMixin, ExportMixin, ImportExportMixin
 from django.http import HttpResponse
 import csv
-
 
 class FileUploadResource(resources.ModelResource):
     class Meta:
@@ -39,7 +38,6 @@ class FileAdmin(ImportExportModelAdmin):
 
     #     return response
 
-    # download_csv.short_description = "Download CSV file for selected profiles."
 
 class MemberResource(resources.ModelResource):
     class Meta:
@@ -51,34 +49,39 @@ class MemeberAdmin(ImportExportModelAdmin):
     list_display = ('member_name','phone_no_secondary_user')
     search_fields = ['member_name','phone_no_secondary_user']
 
+
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('id','image','category','title')
-    search_fields = ['image','category','title']
+    list_display = ('id', 'image', 'category', 'title')
+    search_fields = ['image', 'category', 'title']
+
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('created_by_admin','created_by_primary','created_by_secondary','message')
-    search_fields = ['created_by_admin','created_by_primary','created_by_secondary','message']
+    list_display = ('created_by_admin', 'created_by_primary', 'created_by_secondary', 'message')
+    search_fields = ['created_by_admin', 'created_by_primary', 'created_by_secondary', 'message']
+
 
 class OccupationAdmin(admin.ModelAdmin):
     list_display = ('occupation',)
-    search_fields = ['occupation',]
+    search_fields = ['occupation', ]
+
 
 class FamilyAdmin(admin.ModelAdmin):
     list_display = ('name',)
-    search_fields = ['name',]
+    search_fields = ['name', ]
+
 
 admin.site.register(UserProfile)
 admin.site.register(PrayerGroup)
-admin.site.register(Family,FamilyAdmin)
+admin.site.register(Family, FamilyAdmin)
 admin.site.register(Notice)
 admin.site.register(ChurchDetails)
 admin.site.register(OtpModels)
-admin.site.register(Notification,NotificationAdmin)
-admin.site.register(Images,ImageAdmin)
-admin.site.register(FileUpload,FileAdmin)
-admin.site.register(Members,MemeberAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Images, ImageAdmin)
+admin.site.register(FileUpload, FileAdmin)
+admin.site.register(Members, MemeberAdmin)
 admin.site.register(NoticeBereavement)
-admin.site.register(Occupation,OccupationAdmin)
+admin.site.register(Occupation, OccupationAdmin)
 admin.site.register(MemberType)
 admin.site.register(NoticeReadPrimary)
 admin.site.register(NoticeReadSecondary)
--- church_project/settings.py
+++ church_project/settings.py
@@ -54,6 +54,43 @@ INSTALLED_APPS = [
     'apps.api'
 ]
 
+
+# JET_DEFAULT_THEME = 'green'
+# JET_THEMES = [
+#     {
+#         'theme': 'default', # theme folder name
+#         'color': '#47bac1', # color of the theme's button in user menu
+#         'title': 'Default' # theme title
+#     },
+#     {
+#         'theme': 'green',
+#         'color': '#30a093',
+#         'title': 'Green'
+#     },
+#     {
+#         'theme': 'light-green',
+#         'color': '#2faa60',
+#         'title': 'Light Green'
+#     },
+#     {
+#         'theme': 'light-violet',
+#         'color': '#a464c4',
+#         'title': 'Light Violet'
+#     },
+#     {
+#         'theme': 'light-blue',
+#         'color': '#5EADDE',
+#         'title': 'Light Blue'
+#     },
+#     {
+#         'theme': 'light-gray',
+#         'color': '#222',
+#         'title': 'Light Gray'
+#     }
+# ]
+# JET_CHANGE_FORM_SIBLING_LINKS = True
+
+
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
