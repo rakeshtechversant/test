68682025e606e49fd39e700a450c75fa46e788e9
Midhun PC <midhun.techversant@gmail.com>
2020-02-18 15:14:32 +0530

    primary user name passed

--- apps/api/views.py
+++ apps/api/views.py
@@ -1082,7 +1082,8 @@ class UserDetailView(APIView):
                     'image':image,
                     'primary_in_memory':user_details.primary_user_id.in_memory,
                     'family_id':family_id,
-                    'family_name':family_name
+                    'family_name':family_name,
+                    'primary_user_name':user_details.primary_user_id.name
 
                 }
                 return Response({'success': True,'message':'Profile found successfully','user_details':data}, status=HTTP_200_OK)
@@ -1142,7 +1143,8 @@ class UserDetailView(APIView):
                        'image':image,
                        'relation':user_details.relation,
                        'family_id':family_id,
-                       'family_name':family_name
+                       'family_name':family_name,
+                       'primary_user_name':user_details.name
                     }
                     return Response({'success': True,'message':'Profile found successfully','user_details':data}, status=HTTP_200_OK)
                 except:
