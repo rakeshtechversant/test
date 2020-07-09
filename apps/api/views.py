1fc9fde14c844d149c18c664d1f6e5a7bcc83d48
midhun.pc@techversantinfo.com <midhun.pc@techversantinfo.com>
2020-07-09 12:44:52 +0530

    church history response change

--- apps/api/views.py
+++ apps/api/views.py
@@ -1269,6 +1269,22 @@ class ChurchHistoryEditView(viewsets.ModelViewSet):
     queryset = ChurchDetails.objects.all()
     serializer_class = ChurchHistoryEditSerializer
     permission_classes = [AllowAny]
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        data = {
+                'code': 200,
+                'status': "OK",
+        }
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data['response'] = serializer.data
+        return Response(data)
 
 class ChurchImagesView(RetrieveAPIView):
     queryset = ChurchDetails.objects.all()
